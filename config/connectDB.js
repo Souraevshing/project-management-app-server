2b442f9a6917d31308767920c0ea014aa5385f9a
Sourav Kumar <isauravshing@gmail.com>
2022-07-23 16:02:23 +0530

    Add files via upload

---
+++ config/connectDB.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const connectDB = async () => {
+  try {
+    await mongoose
+      .connect('mongodb+srv://admin:admin@cluster0.gx4hitt.mongodb.net/project-management_db?retryWrites=true&w=majority')
+      .then(() =>
+        console.log(
+          `Connected to MongoDB ${mongoose.connection.host}`.bgGreen.yellow
+            .underline
+        )
+      )
+  } catch (err) {
+    console.log(`${err}`.bgRed.yellow.underline)
+  }
+}
