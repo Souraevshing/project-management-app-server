91605c926ceed821641609bfdbe1bf9449238d46
Sourav Kumar <isauravshing@gmail.com>
2022-07-23 15:45:32 +0530

    Update connectDB.js

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   try {
     await mongoose
-      .connect(process.env.MONGO_URL)
+      .connect('mongodb+srv://admin:admin@cluster0.gx4hitt.mongodb.net/project-management_db?retryWrites=true&w=majority')
       .then(() =>
         console.log(
           `Connected to MongoDB ${mongoose.connection.host}`.bgGreen.yellow
